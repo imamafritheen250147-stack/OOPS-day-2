610c2e622be684807ed04c6c36f8056e9d7255ee
imamafritheen250147-stack <imamafritheen.250147@cse.ritchennai.edu.in>
2026-09-13 18:09:54 +0530

    Create Multiple Derived Classes.java

---
+++ Multiple Derived Classes.java
@@ -0,0 +1,28 @@
+class Shape {
+}
+
+class Circle extends Shape {
+    void area() {
+        double radius = 5;
+        System.out.println("Area of Circle = " + (3.14 * radius * radius));
+    }
+}
+
+class Rectangle extends Shape {
+    void area() {
+        double length = 10;
+        double width = 5;
+
+        System.out.println("Area of Rectangle = " + (length * width));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        Rectangle r = new Rectangle();
+
+        c.area();
+        r.area();
+    }
+}
