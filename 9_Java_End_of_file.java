a2c902a192d02f40eb02dcbec37d9ccddc63d98f
imamafritheen250147-stack <imamafritheen.250147@cse.ritchennai.edu.in>
2026-09-24 18:26:27 +0530

    Create 9_Java_End_of_file.java

---
+++ 9_Java_End_of_file.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int lineNumber = 1;
+
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            System.out.println(lineNumber + " " + line);
+            lineNumber++;
+        }
+
+        scan.close();
+    }
+}
