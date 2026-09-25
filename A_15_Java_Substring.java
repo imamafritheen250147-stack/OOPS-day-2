e1bee9ca7e66aed2bb8296a8b67e260a131851ee
imamafritheen250147-stack <imamafritheen.250147@cse.ritchennai.edu.in>
2026-09-25 20:22:17 +0530

    Create A_15_Java_Substring.java

---
+++ A_15_Java_Substring.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        // Print the substring from index 'start' up to 'end - 1'
+        System.out.println(S.substring(start, end));
+        
+        in.close();
+    }
+}
